892fc6691532d804dcd1d82bb96864210440d8fd
havrylyshyn <>
2020-11-21 16:12:17 +0200

    add test for login

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import time
 import json
 import os.path
 from fixture.application import Application
+import ftputil
 
 fixture = None
 target = None
@@ -17,16 +18,56 @@ def load_config(file):
     return target
 
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
-    web_config = load_config(request.config.getoption("--target"))['web']
-    user = load_config(request.config.getoption("--target"))['webadmin']
+    web_config = config['web']
+    user = config['webadmin']
     browser = request.config.getoption("--browser")
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config['baseUrl'])
     fixture.session.ensure_login(username=user['username'], password=user['password'])
     return fixture
+
+
+@pytest.fixture
+def app_auth(request, config):
+    global fixture
+    web_config = config['web']
+    browser = request.config.getoption("--browser")
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_server_configuration(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    def finn():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+    request.addfinalizer(finn)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
 
 
 @pytest.fixture(scope="session", autouse=True)
--- test/test_login.py
+++ test/test_login.py
@@ -1,4 +1,5 @@
-
-def test_login(app):
-    app.session.login("administrator","root")
-    assert app.session.is_logged_in_as("administrator")
+def test_login(app_auth):
+    if app_auth.session.is_logged_in():
+        app_auth.session.logout()
+    app_auth.session.login("administrator","root")
+    assert app_auth.session.is_logged_in_as("administrator")
